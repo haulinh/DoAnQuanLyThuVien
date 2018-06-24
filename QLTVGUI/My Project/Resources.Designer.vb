1301e32e3fcbba6f3f982d458718605df711d23f
haulinh <haulinhlk@gmail.com>
2018-06-24 10:40:22 +0700

    remove resources

--- QLTVGUI/My Project/Resources.Designer.vb
+++ QLTVGUI/My Project/Resources.Designer.vb
@@ -59,25 +59,5 @@ Namespace My.Resources
                 resourceCulture = value
             End Set
         End Property
-        
-        '''<summary>
-        '''  Looks up a localized resource of type System.Drawing.Bitmap.
-        '''</summary>
-        Friend ReadOnly Property add() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("add", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
-        
-        '''<summary>
-        '''  Looks up a localized resource of type System.Drawing.Bitmap.
-        '''</summary>
-        Friend ReadOnly Property united_states() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("united-states", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
     End Module
 End Namespace
